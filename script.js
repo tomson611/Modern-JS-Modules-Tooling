4bb9e92f3c64b2b83dd6c7172c77b926dd284f2b
Tomson <twesolowski61@gmail.com>
2021-04-08 23:59:23 +0200

    importing and exporting in ES6 modules

---
+++ script.js
@@ -0,0 +1,7 @@
+//Importing module
+// import { addToCart, tq, totalPrice as price } from './shoppingCart.js';
+import * as ShoppingCart from './shoppingCart.js';
+console.log('Importing module');
+
+ShoppingCart.addToCart('bread', 5);
+console.log(ShoppingCart.totalPrice);
