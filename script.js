cc42471e3812c98728ae01a48a755f4fe558b8a8
Tomson <twesolowski61@gmail.com>
2021-04-09 00:07:59 +0200

    default import/export practice

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //Importing module
 // import { addToCart, tq, totalPrice as price } from './shoppingCart.js';
-import * as ShoppingCart from './shoppingCart.js';
-console.log('Importing module');
+// import * as ShoppingCart from './shoppingCart.js';
+// console.log('Importing module');
 
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+import add from './shoppingCart.js';
+add('pizza', 2);
