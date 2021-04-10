1ed7ae87f925acf69d86b6fbbced4c7fdec41874
Tomson <twesolowski61@gmail.com>
2021-04-10 07:33:37 +0200

    practicing the module pattern

--- script.js
+++ script.js
@@ -6,5 +6,28 @@
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice);
 
-import add from './shoppingCart.js';
-add('pizza', 2);
+// import add from './shoppingCart.js';
+// add('pizza', 2);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return addToCart, cart, totalPrice, totalQuantity;
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
