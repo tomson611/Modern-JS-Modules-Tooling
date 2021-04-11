87e88aaf5237d9aaf323c4a02ea6ac79df436d36
Tomson <twesolowski61@gmail.com>
2021-04-11 08:31:21 +0200

    practicing NPM with lodash

--- script.js
+++ script.js
@@ -33,10 +33,27 @@
 // console.log(ShoppingCart2);
 
 // Export
-export.function (product, quantity) {
-        cart.push({ product, quantity });
-        console.log(`${quantity} ${product} added to cart`);
-      };
+// export.function (product, quantity) {
+//         cart.push({ product, quantity });
+//         console.log(`${quantity} ${product} added to cart`);
+//       };
 
-//Import
-const { addTocart} = require('./shoppingCart.js')
+// //Import
+// const { addTocart} = require('./shoppingCart.js')
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
