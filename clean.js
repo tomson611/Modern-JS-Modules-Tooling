25ee9157bbee4b686821e92e2a0bda3365b18f44
Tomson <twesolowski61@gmail.com>
2021-04-13 19:04:43 +0200

    renaming check to checkExpenses for more clarity

--- clean.js
+++ clean.js
@@ -28,7 +28,7 @@ addExpense(100, 'Going to movies 🍿', 'Matilda');
 addExpense(200, 'Stuff', 'Jay');
 console.log(budget);
 
-const check = function () {
+const checkExpenses = function () {
   for (const el of budget) {
     let lim;
     if (spendingLimits[el.user]) {
@@ -42,7 +42,7 @@ const check = function () {
     }
   }
 };
-check();
+checkExpenses();
 
 console.log(budget);
 
